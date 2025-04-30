87b292a7c228b731c336ad43e03c96b1e1982d23
The-Deliverator <mdavoo@gmail.com>
2025-04-30 21:41:43 +0000

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,13 +10,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-     """
-    Asynchronous function that measures and returns the runtime of
-    running 4 instances of the async_comprehension function.
-
-    Returns:
-        float: The runtime of the async_comprehension function in seconds.
-    """
 
     start_time = time.perf_counter()
 
